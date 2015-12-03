fa4bc63c46dafac362484e0306445a067514c310
Gareth Fleming <gareth@joocy.com>
2015-12-03 10:30:02 +0000

    day three, second part.

--- DayThree/deliveries.py
+++ DayThree/deliveries.py
@@ -6,14 +6,11 @@ direction_deltas = {
 }
 
 
-def build_map_for(directions):
-    delivery_map = {(0, 0): 1}
-    current_position = (0, 0)
-    for direction in directions:
-        current_position = tuple([sum(x) for x in zip(current_position, direction_deltas[direction])])
-        count_for_position = delivery_map.get(current_position, 0)
-        delivery_map[current_position] = count_for_position + 1
-    return delivery_map
+def take_turn(current_position, delivery_map, direction):
+    current_position = tuple([sum(x) for x in zip(current_position, direction_deltas[direction])])
+    count_for_position = delivery_map.get(current_position, 0)
+    delivery_map[current_position] = count_for_position + 1
+    return current_position, delivery_map
 
 
 def got_at_least_one_present(directions):
@@ -25,8 +22,33 @@ def got_at_least_one_present(directions):
     >>> got_at_least_one_present("^v^v^v^v^v")
     2
     """
-    return len(build_map_for(directions))
+    current_position = (0, 0)
+    delivery_map = {(0, 0): 1}
+    for direction in directions:
+        current_position, delivery_map = take_turn(current_position, delivery_map, direction)
+    return len(delivery_map)
 
+
+def with_robo_santa(directions):
+    """
+    >>> with_robo_santa("^v")
+    3
+    >>> with_robo_santa("^>v<")
+    3
+    >>> with_robo_santa("^v^v^v^v^v")
+    11
+    """
+    split_directions = ["", ""]
+    for i in range(len(directions)):
+        split_directions[i % 2] += directions[i]
+    santa_directions, robo_directions = split_directions
+    santa_position = (0, 0)
+    robo_position = (0, 0)
+    delivery_map = {(0, 0): 2}
+    for d in range(len(santa_directions)):
+        santa_position, delivery_map = take_turn(santa_position, delivery_map, santa_directions[d])
+        robo_position, delivery_map = take_turn(robo_position, delivery_map, robo_directions[d])
+    return len(delivery_map)
 
 if __name__ == "__main__":
     import doctest
--- README.md
+++ README.md
@@ -3,9 +3,23 @@ My solutions for the adventofcode.com puzzles
 
 ## Day one
 
-The first part of this problem can be solved using a simple reduce. You are, after all, trying to reduce the list of commands into a single value.
+The first part of this problem can be solved using a simple reduce. You are, after all, trying to reduce the list of 
+commands into a single value.
 
-For the second part, I just kept a count of iterations in my reduce and set the basement value to the current count as soon as the floor hit -1.
+For the second part, I just kept a count of iterations in my reduce and set the basement value to the current count as 
+soon as the floor hit -1.
 
 ## Day two
 A couple of simple maths puzzles today. My solutions can very likely be cleaned up quite a bit.
+
+## Day three
+I don't mind telling you that the second bit of this puzzle had me scratching my head for a while. It turns out to be 
+simple enough. I was definitely hampered a little by my solution to the first part which needed a bit of 
+refactoring to work for the second one.
+
+For the first part of the puzzle, we simply need to identify all the houses that are visited. I did this using 
+(x,y) coordinates. This is an arbitrary decision. You could choose any other method. Then it's a matter of modifying
+Santa's current position and keeping a count of all the visits to each coordinate.
+
+The instructions for the second part were a little fuzzy, but it comes down to a bit of a refactor of the first part
+to take turns over the same delivery map.
